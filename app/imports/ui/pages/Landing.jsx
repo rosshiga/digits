5faf1bd092a3cd130b85fbadc9693e1b30bd7eda
Ross H <ross@rosshiga.com>
2018-10-22 23:03:38 -1000

    Fin 1 in 40 mins

--- app/imports/ui/pages/Landing.jsx
+++ app/imports/ui/pages/Landing.jsx
@@ -1,22 +1,31 @@
 import React from 'react';
-import { Grid, Image } from 'semantic-ui-react';
+import { Grid, Image, Icon } from 'semantic-ui-react';
 
 /** A simple static component to render some text for the landing page. */
 class Landing extends React.Component {
   render() {
     return (
-        <Grid verticalAlign='middle' textAlign='center' container>
+        <div className='splash'>
+          <Grid container centered stackable columns={3}>
 
-          <Grid.Column width={4}>
-            <Image size='small' circular src="/images/meteor-logo.png"/>
-          </Grid.Column>
+            <Grid.Column textAlign='center' inverted>
+              <Icon name='users' size='huge' inverted/>
+              <h1>Multiple Users</h1>
+              <h3>This address book enables any number of users to register and save contacts</h3>
+            </Grid.Column>
+            <Grid.Column textAlign='center'>
+              <Icon name='address book' size='huge' inverted/>
+              <h1>Contact Details</h1>
+              <h3>For each contact, you can save their number and name</h3></Grid.Column>
+            <Grid.Column textAlign='center'>
+              <Icon name='calendar' size='huge' inverted/>
+              <h1>Timestamped Notes</h1>
+              <h3>Each time you make a contact with a contact, you can write a note that summarized the conversation.
+                This note is save with the contact and a time stamp.</h3></Grid.Column>
 
-          <Grid.Column width={8}>
-            <h1>Welcome to this template</h1>
-            <p>Now get to work and modify this app!</p>
-          </Grid.Column>
 
-        </Grid>
+          </Grid>
+        </div>
     );
   }
 }
